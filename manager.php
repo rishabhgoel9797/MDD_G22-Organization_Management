7b4a79c7361146f793a1cda70d8a09a980cbc86c
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2018-08-25 23:23:59 +0530

    Feedback Type Chart added

--- manager.php
+++ manager.php
@@ -1,3 +1,9 @@
+<?php
+//die("hi");
+include('connection.php');
+$feedback=mysqli_query($conn,"select feedback_type,count(feedback_type) from employee_data group by feedback_type");
+//die($feedback);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +13,35 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Feedback Type', 'Count(feedback type)'],
+          <?php
+
+          while($row=mysqli_fetch_assoc($feedback))
+          {
+            echo "['".$row['feedback_type']."',".$row['count(feedback_type)']."],";
+          }
+          ?>
+        ]);
+
+        var options = {
+          title: 'Feedback Type given by the employees'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('feedback_type_chart'));
+
+        chart.draw(data, options);
+      }
+      </script>
+
+
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
@@ -45,8 +80,10 @@
 <div class="col-md-6">
  <div class="panel-group">
     <div class="panel panel-primary">
-      <div class="panel-heading">Completed Targets</div>
-      <div class="panel-body text-center"></div>
+      <div class="panel-heading">Feedback Type</div>
+      <div class="panel-body text-center">
+            <div id="feedback_type_chart" ></div>
+      </div>
     </div>
 </div>
 </div>
@@ -55,7 +92,9 @@
  <div class="panel-group">
     <div class="panel panel-warning">
       <div class="panel-heading">Requests from employees</div>
-      <div class="panel-body"></div>
+      <div class="panel-body">
+        
+      </div>
     </div>
 </div>
 </div>
